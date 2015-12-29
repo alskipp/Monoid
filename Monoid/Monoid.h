8fcf216a38ac03a69bb343dfe08b88de71b9fb9e
al_skipp <al_skipp@fastmail.fm>
2015-12-29 20:41:45 +0000

    Initial Commit

---
+++ Monoid/Monoid.h
@@ -0,0 +1,19 @@
+//
+//  Monoid.h
+//  Monoid
+//
+//  Created by Alan Skipp on 29/12/2015.
+//  Copyright © 2015 Al Skipp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Monoid.
+FOUNDATION_EXPORT double MonoidVersionNumber;
+
+//! Project version string for Monoid.
+FOUNDATION_EXPORT const unsigned char MonoidVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Monoid/PublicHeader.h>
+
+
