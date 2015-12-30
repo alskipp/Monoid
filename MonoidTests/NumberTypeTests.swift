b308818fea6aa0de523c6fad745d105d29e7f2e6
al_skipp <al_skipp@fastmail.fm>
2015-12-30 14:09:33 +0000

    Added NumberType tests

---
+++ MonoidTests/NumberTypeTests.swift
@@ -0,0 +1,47 @@
+//
+//  NumberTypeTests.swift
+//  Monoid
+//
+//  Created by Alan Skipp on 30/12/2015.
+//  Copyright © 2015 Al Skipp. All rights reserved.
+//
+
+import XCTest
+import Monoid
+
+class NumberTypeTests: XCTestCase {
+
+  func testZero() {
+    XCTAssert(Int.zero == 0)
+    XCTAssert(Int64.zero == 0)
+    XCTAssert(Int32.zero == 0)
+    XCTAssert(Int16.zero == 0)
+    XCTAssert(Int8.zero == 0)
+    
+    XCTAssert(UInt64.zero == 0)
+    XCTAssert(UInt32.zero == 0)
+    XCTAssert(UInt16.zero == 0)
+    XCTAssert(UInt8.zero == 0)
+    
+    XCTAssert(Double.zero == 0)
+    XCTAssert(Float.zero == 0)
+    XCTAssert(Float80.zero == 0)
+  }
+
+  func testOne() {
+    XCTAssert(Int.one == 1)
+    XCTAssert(Int64.one == 1)
+    XCTAssert(Int32.one == 1)
+    XCTAssert(Int16.one == 1)
+    XCTAssert(Int8.one == 1)
+    
+    XCTAssert(UInt64.one == 1)
+    XCTAssert(UInt32.one == 1)
+    XCTAssert(UInt16.one == 1)
+    XCTAssert(UInt8.one == 1)
+    
+    XCTAssert(Double.one == 1)
+    XCTAssert(Float.one == 1)
+    XCTAssert(Float80.one == 1)
+  }
+}
