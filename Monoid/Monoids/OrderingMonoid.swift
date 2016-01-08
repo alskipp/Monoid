885721b42f80207c7fd38e7b568dbd2ce7d7e0ff
al_skipp <al_skipp@fastmail.fm>
2016-01-08 19:42:26 +0000

    Refactored `Ordering` Monoid

--- Monoid/Monoids/OrderingMonoid.swift
+++ Monoid/Monoids/OrderingMonoid.swift
@@ -7,9 +7,8 @@ extension Ordering: Monoid {
 
   public static func mappend(a: Ordering, _ b: Ordering) -> Ordering {
     switch (a, b) {
-    case (.LT, _): return .LT
     case (.EQ, _): return b
-    case (.GT, _): return .GT
+    default: return a
     }
   }
 }
