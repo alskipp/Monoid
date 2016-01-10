9ac5df24e430f9b73cd91cd8d64084d84bb9427a
al_skipp <al_skipp@fastmail.fm>
2016-01-10 10:56:32 +0000

    Fixed formatting and typos in comments

--- Monoid/Extensions/SequenceType.swift
+++ Monoid/Extensions/SequenceType.swift
@@ -1,8 +1,8 @@
 //  Copyright © 2016 Al Skipp. All rights reserved.
 
-/*:
-Exension to allow sort by **Ordering**
-*/
+
+// Extension to allow sort by **Ordering**
+
 public extension SequenceType {
   func sortBy(@noescape f: (Generator.Element, Generator.Element) -> Ordering) -> [Generator.Element] {
     return sort { f($0, $1) == .LT }
